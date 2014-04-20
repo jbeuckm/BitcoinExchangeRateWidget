e82570496560e89a993bd052f3d2d609c8475089
joe beuckman <joebeuckman@gmail.com>
2014-04-20 10:37:43 -0500

    found an open api

--- source/scripts/rates.js
+++ source/scripts/rates.js
@@ -1,10 +1,16 @@
 
-d3.csv("https://coinbase.com/api/v1/prices/historical")
-  .row(function(d) { return {key: d.key, value: +d.value}; })
+//  d3.csv("https://coinbase.com/api/v1/prices/historical")
+
+
+//d3.csv("https://api.bitcoinaverage.com/ticker/USD/")
+d3.csv("https://api.bitcoinaverage.com/history/USD/per_minute_24h_sliding_window.csv")
+  .row(function(d) {
+
+    console.log(d);
+    d3.select(".helloText").html(d.average);
+
+  })
   .get(function(error, rows) {
-
-    d3.select(".helloText").html('hi');
-
     console.log(rows);
   });
 
