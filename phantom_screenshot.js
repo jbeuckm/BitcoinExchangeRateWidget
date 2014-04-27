54c8c8c7b9a3d6117101b9a00030e11453c48307
joe beuckman <joebeuckman@gmail.com>
2014-04-27 14:51:21 -0500

    screenshot

--- phantom_screenshot.js
+++ phantom_screenshot.js
@@ -3,6 +3,10 @@
 var page = require('webpage').create();
 
 page.open('source/index.html', function() {
-  page.render('source/Default.png');
-  phantom.exit();
+
+  setTimeout(function(){
+    page.render('source/Default.png');
+    phantom.exit();
+  }, 2000);
+
 });
